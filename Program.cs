c45cfeb41d6931537c5c540ace009d3310ec6ffc
brandon <49256014+merc-ury@users.noreply.github.com>
2019-09-11 19:24:39 -0400

    Add files via upload
    c# shell using IronPython
    don't forget to add reference when using

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using IronPython.Hosting;
+
+namespace CSharpShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipy = Python.CreateRuntime();
+            dynamic pythonFile = ipy.UseFile("test.py");
+
+            // Passing data into a Python function
+            object[] data = {
+                "String 1",
+                100,
+                true,
+                10.03,
+                'c'
+            };
+            pythonFile.call(data);
+
+            // Retrieving data from a getter function
+            Console.WriteLine();
+            string name = pythonFile.getName();
+            Console.WriteLine(name);
+
+
+            // Passing user-inputed data to a Python function
+            Console.WriteLine();
+            string msg = Console.ReadLine();
+            Console.WriteLine(pythonFile.printMsg(msg));
+
+            // Squaring number
+            Console.WriteLine();
+            Console.WriteLine(pythonFile.squareNum(100));
+
+        }
+    }
+}
---
+++ test.py
@@ -0,0 +1,13 @@
+def call(data):
+    print("This loop is executed in Python")
+    for i in data:
+        print(i)
+
+def getName():
+    return "Brandon"
+
+def printMsg(msg):
+    return "This is called from Python and your message is: " + msg
+
+def squareNum(x):
+    return x * x
